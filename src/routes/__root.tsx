86789c1090fc95c5c8b5a9e58749f537324fa3fe
gene george <genewgeorge@gmail.com>
2026-03-27 09:31:24 -0400

    Refactor RootComponent and add SEO schema script
    Refactor RootComponent to use functional component syntax and add main wrapper for Outlet.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,19 +1,35 @@
-import { HeadContent, Scripts, createRootRoute, Outlet } from '@tanstack/react-router'
-import { Header } from '../components/Header'
-import { Footer } from '../components/Footer'
-import '../styles.css' // <--- THIS LINE MUST BE HERE
+import { createRootRoute, Outlet } from '@tanstack/react-router'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export const Route = createRootRoute({
-  component: RootComponent,
-})
-
-function RootComponent() {
-  return (
+  component: () => (
     <>
       <Header />
-      <Outlet />
+      <main className="min-h-screen bg-[#0a0a0a]">
+        <Outlet />
+      </main>
       <Footer />
-      <Scripts />
+      
+      {/* GLOBAL SEO SCHEMA */}
+      <script type="application/ld+json" dangerouslySetInnerHTML={{
+        __html: JSON.stringify({
+          "@context": "https://schema.org",
+          "@type": "LocalBusiness",
+          "name": "J. Worden & Sons Asphalt Paving",
+          "telephone": "804-446-1296",
+          "url": "https://jwordenasphaltpaving.com",
+          "address": {
+            "@type": "PostalAddress",
+            "addressLocality": "Richmond",
+            "addressRegion": "VA",
+            "postalCode": "23221",
+            "addressCountry": "US"
+          },
+          "areaServed": "Virginia 41-City Grid",
+          "description": "Premium asphalt paving and sealcoating with a 6-inch structural stone base standard."
+        })
+      }} />
     </>
-  )
-}
+  ),
+})
