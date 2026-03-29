f5cb2d4149ea90066c4849c749b9a6c06e4a7b3d
gene george <genewgeorge@gmail.com>
2026-03-29 13:15:57 -0400

    Refactor Vite config to streamline plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,12 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-const config = defineConfig({
+export default defineConfig({
   plugins: [
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tailwindcss(),
-    netlify(),
+    tsconfigPaths(),
     tanstackStart(),
-    viteReact(),
+    react(),
   ],
 })
-
-export default config
