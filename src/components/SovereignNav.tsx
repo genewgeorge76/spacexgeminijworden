d0ea3df8f262a187f8f9e9a74c44aa81b8c7f85d
gene george <genewgeorge@gmail.com>
2026-04-13 03:01:37 -0400

    Create SovereignNav component

---
+++ src/components/SovereignNav.tsx
@@ -0,0 +1,17 @@
+// Hub-and-Spoke Navigation Component
+import React from 'react';
+
+const SovereignNav = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Hub</li>
+                <li>Spoke 1</li>
+                <li>Spoke 2</li>
+                <li>Spoke 3</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default SovereignNav;
